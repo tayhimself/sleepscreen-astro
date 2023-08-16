3c9e094e2ce413a95e61e414293a731c841b46e4
Salim Qadri <sqadri@rics.bwh.harvard.edu>
2023-08-16 12:25:57 -0400

    Main layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,25 @@
-import { defineConfig } from 'astro/config';
+import path from "path";
+import { fileURLToPath } from "url";
+
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+
+import { SITE } from "./src/config.mjs"
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: SITE.origin,
+  base: SITE.basePathname,
+  trailingSlash: SITE.trailingSlash ? "always" : "never",
+  output: "static",
+  integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "./src"),
+      },
+    },
+  },
+})
---
+++ src/config.mjs
@@ -0,0 +1,15 @@
+export const SITE = {
+	name: 'MGB Sleep Disorder Screening for Railroaders',
+	origin: 'https://sleepscreen.',
+	basePathname: '/',
+  trailingSlash: false,
+
+	title: 'Anonymous Sleep Disorder Screening for Railroaders',
+	description: 'An anonymous sleep disorder screening for railroaders developed for the FRA.',
+  // These are for the Open Graph tags
+  og_type: 'Research education resource sleep',
+  og_image: '/images/opengraph.png',
+  og_image_alt: 'Anonymous sleep disorder screening for railroaders',
+  noindex: false,
+  nofollow: true,
+};
